738a71fb9265f32b0cc8040d7aa030f74f7c735f
Franchesca Marie Donadillo <franchescamariedonadillo@gmail.com>
2023-04-25 11:28:51 +0800

    open files

--- squared even-cube odd.py
+++ squared even-cube odd.py
@@ -5,6 +5,8 @@
 # read source text file integers.txt
 # create file named double.txt
 # create file triple.txt
+with open("integers.txt", "r") as file_integers, open("double.txt", "w") as even_square, open("triple.txt", "w"):
+    
 # iterate through integers.txt
 # determine the even numbers and square it
 # determine the odd numbers and cube it
