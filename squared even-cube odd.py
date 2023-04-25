cc4b962bffcbfdff0d32eaf7a270be241125de37
Franchesca Marie Donadillo <franchescamariedonadillo@gmail.com>
2023-04-25 11:46:09 +0800

    working cube odd

--- squared even-cube odd.py
+++ squared even-cube odd.py
@@ -5,14 +5,17 @@
 # read source text file integers.txt
 # create file named double.txt
 # create file triple.txt
-with open("integers.txt", "r") as file_integers, open("double.txt", "w") as even_square, open("triple.txt", "w"):
+with open("integers.txt", "r") as file_integers, open("double.txt", "w") as even_square, open("triple.txt", "w") as cube_odd:
 
     # iterate through integers.txt
     for line in file_integers:
         num = int(line)
         # determine the even numbers and square it
         if num % 2 == 0:
-            sqrt = (num)**2
-            even_square.write(str(sqrt) + "\n")
+            sq_even = (num)**2
+            even_square.write(str(sq_even) + "\n")
         
         # determine the odd numbers and cube it
+        elif num % 2 == 1:
+            cube = (num)**3
+            cube_odd.write(str(cube) + "\n")
