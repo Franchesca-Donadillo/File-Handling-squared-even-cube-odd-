125a33315274354b125d7b99a94cb2de32699ece
Franchesca Marie Donadillo <franchescamariedonadillo@gmail.com>
2023-04-25 11:07:13 +0800

    pseudocode

---
+++ squared even-cube odd.py
@@ -0,0 +1,10 @@
+# Franchesca Marie U. Donadillo
+# BSCPE 1-5
+# Program 4 - Squared even, cube odd
+
+# read source text file integers.txt
+# create file named double.txt
+# create file triple.txt
+# iterate through integers.txt
+# determine the even numbers and square it
+# determine the odd numbers and cube it
